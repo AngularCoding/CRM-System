08b03b3e7edf51fc989050a51557ccc2a8687c63
Mykola Matsko <mykola.matsko@gmail.com>
2020-08-09 00:36:12 +0300

    first route

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const express = require('express')
-const app = express()
-module.exports = app
+const express = require('express');
+const authRoutes = require('./routes/auth');
+const app = express();
+
+app.use('/api/auth', authRoutes);
+
+
+module.exports = app;
