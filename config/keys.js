ab9be13735a149fe53ee1f50e1344b8fe1679f76
Mykola Matsko <mykola.matsko@gmail.com>
2020-08-10 15:28:13 +0300

    login and register added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://matsko:Welcome123@fullstack.wanc6.mongodb.net/fullstack?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://matsko:Welcome123@fullstack.wanc6.mongodb.net/fullstack?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,59 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
 
-module.exports.login = function (req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
-        }
+module.exports.login = async function (req, res) {
+  const candidate = await User.findOne({email: req.body.email});
+
+  if (candidate) {
+  const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+    if (passwordResult) {
+      //generate token
+      const token = jwt.sign({
+          email: candidate.email,
+          userId: candidate._id
+      }, keys.jwt, {expiresIn: 60 * 60});
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+    } else {
+      res.status(401).json({
+        message: 'Password not match'
+      })
+    }
+  } else {
+    res.status(404).json({
+      message: 'User not found'
     })
+  }
 };
 
-module.exports.register = function (req, res) {
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
+module.exports.register = async function (req, res) {
+  //find email with user
+  const candidate = await User.findOne({
+    email: req.body.email,
+  });
 
-    user.save().then(() => console.log('User created'))
+  if (candidate) {
+    //user is created throw error
+    res.status(409).json({
+      message: "This email is used",
+    });
+  } else {
+    const salt = bcrypt.genSaltSync(10,);
+    const password = req.body.password;
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt),
+    });
+
+    try {
+      await user.save();
+      res.status(201).json(user)
+    } catch (e) {
+      //Error display
+    }
+  }
 };
