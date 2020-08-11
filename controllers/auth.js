79f5d5c75605d60bb486ba42ac0a6dc30d02de41
Mykola Matsko <mykola.matsko@gmail.com>
2020-08-11 09:45:17 +0300

    endpoints added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');
+const errorHandler = require('../utils/errorHandler');
 
 module.exports.login = async function (req, res) {
   const candidate = await User.findOne({email: req.body.email});
@@ -54,6 +55,7 @@ module.exports.register = async function (req, res) {
       res.status(201).json(user)
     } catch (e) {
       //Error display
+      errorHandler(res, e)
     }
   }
 };
--- routes/order.js
+++ routes/order.js
@@ -2,8 +2,8 @@ const express = require('express');
 const controller = require('../controllers/order');
 const router = express.Router();
 
-router.post('/', controller.getAll);
-router.get('/', controller.create);
+router.post('/', passport.authenticate('jwt', {session: false}), controller.getAll);
+router.get('/', passport.authenticate('jwt', {session: false}), controller.create);
 
 module.exports = router;
 
